83ba0f9e40e2899219239de32ef43d918167d4a4
Abner <contact@abnersn.dev>
2025-03-04 20:25:37 -0300

    chore: prepare build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import Icons from 'unplugin-icons/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://danilreis.netlify.com',
   vite: {
+    server: {
+      allowedHosts: true,
+    },
     plugins: [
       Icons({
         compiler: 'astro',
